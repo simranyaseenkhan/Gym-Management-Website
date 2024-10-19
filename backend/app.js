9d27e435d1c38c8b5876c48fd64f7b5fa9378aac
Simran Khan <149397008+simranyaseenkhan@users.noreply.github.com>
2024-10-19 12:56:48 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,66 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import { sendEmail } from "./utils/sendEmail.js";
+
+const app = express();
+const router = express.Router();
+
+config({ path: "./config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+router.post("/send/mail", async (req, res, next) => {
+  const { name, email, message } = req.body;
+  if (!name || !email || !message) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all details",
+    });
+  }
+
+  try {
+    await sendEmail({
+      email: "khansimran.0429@gmail.com",
+      subject: "FitFusion Contact",
+      message,
+      userEmail: email,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Message Sent Successfully.",
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+});
+
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
